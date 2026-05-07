d20bd580fbb18438f29d457fe10fb87233fb99e0
Sumi <38939520+sumimaria@users.noreply.github.com>
2026-05-07 12:16:45 +0530

    Add event listener for issued events

--- src/events.js
+++ src/events.js
@@ -1,1 +1,18 @@
+import "dotenv/config";
+import chalk from "chalk";
+import instance from "./ethers.js";
+
+(() => {
+  console.log(chalk.magenta("Listening for Issue Events..."));
+  console.log(chalk.cyan("Contract: " + instance.target));
+
+  instance.on("Issued", (course, id, grade, event) => {
+    console.log(chalk.bgGreen("**** EVENT OCCURED ****"));
+    console.log("course:", course);
+    console.log("id:", id.toString());
+    console.log("grade:", grade);
+    console.log("tx:", event.log.transactionHash);
+    console.log(chalk.bgGreen("***********************"));
+  });
+})();
 
